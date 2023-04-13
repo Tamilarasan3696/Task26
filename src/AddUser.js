97814f4db94cca84236cdccecd5141eb271eb729
Tamilarasan <115761431+Tamilarasan3696@users.noreply.github.com>
2023-04-13 18:03:33 +0545

    Add files via upload

---
+++ src/AddUser.js
@@ -0,0 +1,136 @@
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import "yup-phone";
+
+import axios from "axios";
+
+
+const userValidationSchema = yup.object({
+    poster: yup.string()
+        .min(4, "Paste a valid url")
+        .required("Image is mandatory "),
+    name: yup.string()
+        .min(2, "Please enter valid name")
+        .required("Name is mandatory "),
+   
+    email: yup.string()
+        .min(10, "Please enter valid email")
+        .required("Email is mandatory "),
+    
+    contact: yup.string().phone("IN").required('Number is mandatory')
+    
+});
+
+export default function AddMember({ detail, setDetail }) {
+    const formik = useFormik({
+        initialValues: {
+            poster: "",
+            name: "",
+            email: "",
+            contact: "",
+            id:"",
+        },
+        validationSchema: userValidationSchema,
+        onSubmit: (newUser) => {
+            createUser(newUser);
+        },
+    });
+    const navigate = useNavigate();
+
+    const createUser = (newUser) => {
+        console.log("createUser", newUser);
+        axios.post("https://6437ab94894c9029e8c2db12.mockapi.io/users",(newUser))
+        .then((res)=>{console.log(res.data)
+        navigate('/')
+        setDetail([...detail,newUser])
+        })
+        
+    };
+    return (
+        <form onSubmit={formik.handleSubmit} style={{ marginLeft: '100px', marginTop: '50px' }} className="add-user-form">
+            <h1 style={{ color: "green", fontWeight: 'bolder'}}>Fill the below details to add a Member</h1><br />
+            <TextField
+                id="poster"
+                name="poster"
+                label="Image"
+                variant="outlined"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.poster}
+                style={{ width: '800px' }}
+            /><br />
+            {formik.touched.poster && formik.errors.poster ? formik.errors.poster : ""}
+            <br />
+            <TextField
+                id="name"
+                name="name"
+                label="name"
+                variant="outlined"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.name}
+                style={{ width: '800px' }}
+            /><br />
+            {formik.touched.name && formik.errors.name
+                ? formik.errors.name
+                : ""}
+            <br />
+        
+            <TextField
+                id="email"
+                name="email"
+                label="email"
+                variant="outlined"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                style={{ width: '800px' }}
+            /><br />
+            {formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : ""}
+          <br/>
+            <TextField
+                id="contact"
+                name="contact"
+                label="contact"
+                variant="outlined"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.contact}
+                style={{ width: '800px' }}
+            /><br />
+            {formik.touched.contact && formik.errors.contact
+                ? formik.errors.contact
+                : ""}
+            <br />
+            <TextField
+                id="id"
+                name="id"
+                label="id"
+                variant="outlined"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.id}
+                style={{ width: '800px' }}
+            /><br />
+            {formik.touched.id && formik.errors.id
+                ? formik.errors.id
+                : ""}
+            <br />
+            <Button
+                type="submit"
+                variant="contained"
+                color="success"
+                onClick={createUser}
+            >
+                Add New User
+            </Button>
+
+            
+        </form>
+    );
+}
---
+++ src/EditUser.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import axios from "axios";
+
+
+export default function EditMember() {
+    const { userid } = useParams();
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        axios.get(`
+        https://6437ab94894c9029e8c2db12.mockapi.io/users/${userid}`)
+            .then((us) => { setUser(us.data) })
+    });
+    return user ? <EditUserForm user={user} setUser={setUser}/> : "Please wait........!!!"
+}
+
+function EditUserForm({ user}) {
+    const [poster, setPoster] = useState(user.poster);
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [contact, setContact] = useState(user.contact);
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="add-user-form" style={{ marginLeft: '50px', marginTop: '50px' }}>
+            <h1 style={{ color: "green", fontWeight: 'bolder', fontFamily: 'cursive' }}>Edit and Save ✨</h1><br />
+            <TextField
+                label="image"
+                variant="outlined"
+                onChange={(event) => setPoster(event.target.value)}
+                type="text"
+                placeholder="Enter image url"
+                value={poster}
+                style={{ width: '800px' }}
+            /><br /><br />
+            <TextField
+                label="Name"
+                variant="outlined"
+                onChange={(event) => setName(event.target.value)}
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                style={{ width: '800px' }}
+            /><br /><br />
+           
+            <TextField
+                label="Email"
+                variant="outlined"
+                onChange={(event) => setEmail(event.target.value)}
+                type="text"
+                placeholder="Enter valid email"
+                value={email}
+                style={{ width: '800px' }}
+            /><br /><br />
+           
+            <TextField
+                label="Contact number"
+                variant="outlined"
+                onChange={(event) => setContact(event.target.value)}
+                type="text"
+                placeholder="Enter your Contact Number"
+                value={contact}
+                style={{ width: '800px' }}
+            /><br /><br />
+            <Button
+                color="success"
+                variant="contained"
+                onClick={
+                    () => {
+                        const updatedUser = {
+                            poster: poster,
+                            name: name,
+                            email: email,
+                            contact: contact,
+                        };
+                        
+                        fetch(` https://6437ab94894c9029e8c2db12.mockapi.io/users/${user.id}`, {
+                            method: "PUT",
+                            body: JSON.stringify(updatedUser),
+                            headers: { "Content-Type": "application/json" },
+                        })
+                            .then((data)=>data.json())
+                            .then(()=>navigate("/"));
+                        }
+                }
+            >
+            SAVE USER
+        </Button>
+
+    <Button color="success"
+        variant="contained"
+        onClick={() => { navigate(-1) }}
+    >
+        <ArrowBackIosIcon /> BACK
+    </Button>
+        </div >
+    )
+}
